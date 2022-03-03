d271d7273294dd17fb3af21e059badbf3336316c
Ahmed Kenawy <a7medsa3dkenawy@gmail.com>
2022-03-04 00:24:05 +0200

    Design Food Recipes Fragment and Handle Shimmer Effect

--- app/src/main/java/com/a7medkenawy/foody/FoodRecipiesFragment.kt
+++ app/src/main/java/com/a7medkenawy/foody/FoodRecipiesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_food_recipies.view.*
 
 
 class FoodRecipiesFragment : Fragment() {
@@ -15,7 +16,11 @@ class FoodRecipiesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_recipies, container, false)
+        val view= inflater.inflate(R.layout.fragment_food_recipies, container, false)
+
+        view.shimmer_recycler_view.showShimmer()
+
+        return view
     }
 
 
