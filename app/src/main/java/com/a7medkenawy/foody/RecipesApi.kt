8c5331257c8f05e47025511f55f5ce967f09c1ae
Ahmed Kenawy <a7medsa3dkenawy@gmail.com>
2022-03-06 19:19:57 +0200

    Handle Response

--- app/src/main/java/com/a7medkenawy/foody/RecipesApi.kt
+++ app/src/main/java/com/a7medkenawy/foody/RecipesApi.kt
@@ -1,9 +1,11 @@
 package com.a7medkenawy.foody
 
+import com.a7medkenawy.foody.models.FoodRecipe
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
 
 interface RecipesApi {
     @GET("/recipes/complexSearch")
-    suspend fun getRecipes(@QueryMap map: Map<String,String>)
+    suspend fun getRecipes(@QueryMap map: Map<String,String>):Response<FoodRecipe>
 }
