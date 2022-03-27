59dbe02cbc30522a84d40a89c047c499c36fbc93
Ahmed Kenawy <a7medsa3dkenawy@gmail.com>
2022-03-27 20:47:05 +0200

    handle Data Store Preferences

--- app/src/main/java/com/a7medkenawy/foody/util/Constants.kt
+++ app/src/main/java/com/a7medkenawy/foody/util/Constants.kt
@@ -3,19 +3,32 @@ package com.a7medkenawy.foody.util
 class Constants {
 
     companion object {
-        const val BASE_URL="https://api.spoonacular.com"
-        const val API_KEY="4eb37aeef2594750966f3ec47bc31e49"
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "4eb37aeef2594750966f3ec47bc31e49"
 
         //API Query Map
-        const val QUERY_NUMBER="number"
-        const val QUERY_API_KEY="apiKey"
-        const val QUERY_TYPE="type"
-        const val QUERY_DIET="diet"
-        const val QUERY_ADD_RECIPE_INFORMATION="addRecipeInformation"
-        const val QUERY_FILL_INGREDIENTS="fillIngredients"
+        const val QUERY_NUMBER = "number"
+        const val QUERY_API_KEY = "apiKey"
+        const val QUERY_TYPE = "type"
+        const val QUERY_DIET = "diet"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+        const val QUERY_FILL_INGREDIENTS = "fillIngredients"
 
         //Room
-        const val RECIPES_DATABASE="recipes_database"
-        const val RECIPES_TABLE="recipes_table"
+        const val RECIPES_DATABASE = "recipes_database"
+        const val RECIPES_TABLE = "recipes_table"
+
+        //Bottom Sheet and Preferences
+        const val DEFAULT_MEAL_TYPE = "main_course"
+        const val DEFAULT_DIET_TYPE = "gluten free"
+        const val DEFAULT_RECIPES_NUMBER = "50"
+
+        const val PREFERENCES_NAME="foody_preferences"
+        const val SELECTED_MEAL_TYPE = "selectedMealType"
+        const val SELECTED_MEAL_TYPE_ID = "selectedMealTypeId"
+        const val SELECTED_DIET_TYPE = "selectedDietType"
+        const val SELECTED_DIET_TYPE_ID = "selectedDieTypeId"
+
+
     }
 }
--- app/src/main/java/com/a7medkenawy/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/a7medkenawy/foody/viewmodels/RecipesViewModel.kt
@@ -2,19 +2,55 @@ package com.a7medkenawy.foody.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.a7medkenawy.foody.data.DataStoreRepository
 import com.a7medkenawy.foody.util.Constants
+import com.a7medkenawy.foody.util.Constants.Companion.API_KEY
+import com.a7medkenawy.foody.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.a7medkenawy.foody.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.a7medkenawy.foody.util.Constants.Companion.DEFAULT_RECIPES_NUMBER
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class RecipesViewModel(application: Application) : AndroidViewModel(application) {
+@HiltViewModel
+class RecipesViewModel @Inject constructor(
+    application: Application,
+    private val dataStoreRepository: DataStoreRepository
+) : AndroidViewModel(application) {
+
+    var mealType = DEFAULT_MEAL_TYPE
+    var dietType = DEFAULT_DIET_TYPE
+    val readMealAndType = dataStoreRepository.readMealAndType
+
+    fun saveMealAndType(
+        mealType: String,
+        mealTypeId: Int,
+        dietType: String,
+        dietTypeId: Int
+    ) {
+        viewModelScope.launch {
+            dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
+        }
+    }
+
     fun applyQueries(): Map<String, String> {
         val queries = HashMap<String, String>()
 
-        queries[Constants.QUERY_NUMBER] = "50"
-        queries[Constants.QUERY_API_KEY] = Constants.API_KEY
-        queries[Constants.QUERY_TYPE] = "snack"
-        queries[Constants.QUERY_DIET] = "vegan"
+        viewModelScope.launch {
+            readMealAndType.collect { value ->
+                mealType = value.selectedMealType
+                mealType = value.selectedMealType
+            }
+        }
+
+        queries[Constants.QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
+        queries[Constants.QUERY_API_KEY] = API_KEY
+        queries[Constants.QUERY_TYPE] = DEFAULT_MEAL_TYPE
+        queries[Constants.QUERY_DIET] = DEFAULT_DIET_TYPE
         queries[Constants.QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[Constants.QUERY_FILL_INGREDIENTS] = "true"
-
         return queries
     }
 }
