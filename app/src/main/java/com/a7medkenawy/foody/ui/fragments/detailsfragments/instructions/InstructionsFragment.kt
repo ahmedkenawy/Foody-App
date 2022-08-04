3d7c54551010e4c7a3dae16d3cd776edb41ec574
Ahmed Kenawy <a7medsa3dkenawy@gmail.com>
2022-08-04 21:40:42 +0200

    instruction Fragment

--- app/src/main/java/com/a7medkenawy/foody/ui/fragments/detailsfragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/a7medkenawy/foody/ui/fragments/detailsfragments/instructions/InstructionsFragment.kt
@@ -5,18 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.a7medkenawy.foody.R
+import com.a7medkenawy.foody.databinding.FragmentInstructionsBinding
+import com.a7medkenawy.foody.models.Result
 
 
 class InstructionsFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentInstructionsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        binding = FragmentInstructionsBinding.inflate(layoutInflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable("recipesBundle")
+
+        binding.instructionWebview.webViewClient = object : WebViewClient() {}
+        binding.instructionWebview.loadUrl(myBundle!!.sourceUrl!!)
+
+
+
+        return binding.root
     }
 
 }
